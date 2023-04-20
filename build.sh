6a2cd221181002ccc2793baba2094e1afa98933a
Ian Jones <ianjones834@protonmail.com>
2023-04-20 13:32:51 -0400

    fixed compile/run bug, reorganized directories, finished template

---
+++ build.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+cppFilePath="/workspaces/competitive-programming/src"
+
+/bin/g++ -Wall -std=c++11 "${cppFilePath}/main.cpp" -o ./src/a.out
--- run.sh
+++ run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+./build.sh
+
 cppFilePath="/workspaces/competitive-programming/src"
 inputFilePath="/workspaces/competitive-programming/src/input"
 outputFilePath="/workspaces/competitive-programming/src/output"
 
 > $outputFilePath/actual.txt
 
-/bin/g++ -Wall -std=c++11 "${cppFilePath}/main.cpp" -o a.out && ./a.out < $inputFilePath/input.txt >> "${outputFilePath}/actual.txt"
-diff "${outputFilePath}/actual.txt" "${outputFilePath}/expected.txt"
+./src/a.out < $inputFilePath/input.txt >> "${outputFilePath}/actual.txt"
+
+diff -Z "${outputFilePath}/actual.txt" "${outputFilePath}/expected.txt"
 
 rm "${cppFilePath}/a.out"
