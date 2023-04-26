07cba34474e5330c8467e60aa7bef85f9bf533ea
Ian Jones <70075883+ianjones834@users.noreply.github.com>
2023-04-26 14:24:39 -0400

    Removed the test functionality

--- test.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-source .filepath.sh
-
-./build.sh
-> $outputFilePath/actual.txt
-
-./src/main < $inputFilePath/input.txt >> "${outputFilePath}/actual.txt"
-diff -Z "${outputFilePath}/actual.txt" "${outputFilePath}/expected.txt"
